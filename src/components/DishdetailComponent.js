d77ec7be5929ee9a783facf4427cc422c3fe3f36
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2021-10-12 22:34:11 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,7 @@ import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const maxlength = (len) => (val) => !(val) || (val.length <= len);
 const minlength = (len) => (val) => val && (val.length >= len);
@@ -15,13 +16,15 @@ const minlength = (len) => (val) => val && (val.length >= len);
 function RenderDish({ dish }) {
     if (dish != null) {
         return (
-            <Card>
-                <CardImg src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in transformProps={{ exitTransform: 'scale(0.5) translateY(-50%)' }}>
+                <Card>
+                    <CardImg src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     } else {
         return <div></div>;
@@ -33,13 +36,18 @@ function RenderComment({ comments, postComment, dishId }) {
     if (comments != null) {
         return (
             <div>
-                {comments.map((comment) => (
-                    <div key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
-                ))}
-
+                <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            </Fade>
+                        );
+                    })}
+                </Stagger>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
@@ -155,7 +163,7 @@ const DishDetail = (props) => {
                 </div>
             </div>
         );
-    } else if(props.dish != null){
+    } else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
